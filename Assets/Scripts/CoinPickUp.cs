7bed02497e7b54efb7cf0019e09ebbe3a227cb9d
Mert Dalkılıç <116025152+MrTractorWheel@users.noreply.github.com>
2024-08-02 12:12:29 +0300

    fix(sound): Minor fixes forgotten before

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
-    [SerializeField] AudioClip coinPickupSFX;
     [SerializeField] int pointsForPickup = 100;
     bool wasCollected = false;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player") && !wasCollected){
-           FindObjectOfType<VolumeSettings>().playCoinPickUpSFX();
+            FindObjectOfType<VolumeSettings>().playCoinPickUpSFX();
             wasCollected = true;
             FindObjectOfType<GameSession>().ToScore(pointsForPickup);
             Destroy(gameObject);
