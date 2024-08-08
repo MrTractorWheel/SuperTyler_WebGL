bba9d44f3090dc09b5cffcbfa393fdf38b4caed5
Mert Dalkılıç <116025152+MrTractorWheel@users.noreply.github.com>
2024-08-08 15:05:25 +0300

    fix(UI): Minor adjustments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
